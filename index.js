988beaaeca184fa1ba3dbb90fc1f81b3c6ec6141
Princearman777 <sarkerarman25@gmail.com>
2024-01-19 00:48:24 +0600

    initially create post,get,delete method

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://offline-sharing-service.surge.sh/",
-    ],
+    origin: ["https://offline-sharing-service.surge.sh/"],
     credentials: true,
   })
 );
@@ -21,7 +19,6 @@ console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pahimj1.mongodb.net/?retryWrites=true&w=majority`;
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,26 +26,62 @@ const client = new MongoClient(uri, {
     strict: true,
 
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    // Database Collection 
+    const NewsCollection = client.db("NewsDb").collection("AllNews")
+
+    // Use Post Method
+
+    //   Create News Section
+    app.post("/News", async (req, res) => {
+      const News = req.body;
+      console.log("News ", News);
+      const result = await NewsCollection.insertOne(News);
+      console.log(result);
+      // res.send(result);
+      res.send(result);
+    });
+
+
+// Use Get Method
+
+
+app.get("/AllNews",  async (req, res) => {
+  const result = await NewsCollection.find().toArray();
+  res.send(result);
+});
+
+// Use Delete Method
+
+
+app.delete("/AllNews/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await NewsCollection.deleteOne(query);
+  res.send(result);
+});
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
- app.get('/',(req,res)=>{
-  res.send("The Morning Server Is Running")
- })
-app.listen(port,()=>{
-  console.log(`Server is Running On ${port}` );
-})
+app.get("/", (req, res) => {
+  res.send("The Morning Server Is Running");
+});
+app.listen(port, () => {
+  console.log(`Server is Running On ${port}`);
+});
