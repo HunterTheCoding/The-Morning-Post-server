6d86bbf48559cdae9dec335059700c32d6e5c7a6
Princearman777 <sarkerarman25@gmail.com>
2024-02-07 13:02:42 +0600

    check cors provlem

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
+      "http://localhost:5173",
+      // "http://localhost:5174",
       "https://the-morning-posts.surge.sh",
     ],
     credentials: true,
