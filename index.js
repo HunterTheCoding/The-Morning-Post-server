753c2a996e65e773a5a23e9b5b507e522991fe5a
Princearman777 <sarkerarman25@gmail.com>
2024-02-15 00:58:05 +0600

    complete survey and poll api

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ async function run() {
     const JobsCollection = client.db("NewsDb").collection("AllJobs");
     const UserCollection = client.db("NewsDb").collection("Users");
     const BookmarksCollection = client.db("NewsDb").collection("BookMark");
+    const PullCollection = client.db("NewsDb").collection("Survey-Pull");
     const DonationRequestCollection = client
       .db("NewsDb")
       .collection("Donation");
@@ -168,6 +169,49 @@ async function run() {
       });
     });
 
+    //  create job
+    app.post("/api/v1/jobs", async (req, res) => {
+      try {
+        const body = req.body;
+        const result = await JobsCollection.insertOne(body);
+        res.send(result);
+      } catch (error) {
+        console.error("Error from post api job:", error);
+        res.status(500).send("Internal server error from post api job");
+      }
+    });
+
+    // Create Poll Request
+    // Route to create a new poll
+    app.post("/Polls", async (req, res, next) => {
+      try {
+        const { body } = req;
+
+        // Check if request body is valid
+        if (!body) {
+          return res.status(400).json({ message: "Invalid body" });
+        }
+
+        // Prepare new poll object with default values
+        const newPoll = {
+          ...body,
+          options: body.options.map((option) => ({
+            ...option,
+            _id: new ObjectId(),
+            votes: [],
+          })),
+        };
+
+        // Insert the new poll into the database
+        const createdPoll = await PullCollection.insertOne(newPoll);
+        console.log(createdPoll, newPoll);
+        res.send(createdPoll);
+      } catch (error) {
+        // Pass any errors to the error handling middleware
+        next(error);
+      }
+    });
+
     // Use Get Method
 
     // donation details show
@@ -202,7 +246,7 @@ async function run() {
       // console.log(result);
       res.send(result);
     });
-//  news 
+    //  news
 
     app.get("/News/:section?", async (req, res) => {
       if (req.params.section) {
@@ -230,10 +274,15 @@ async function run() {
       // console.log(result);
       res.send(result);
     });
-    // Conne
 
+    //  get  All Pull request
+    app.get("/Show-Pull", verifyToken, async (req, res) => {
+      const AllPUll = await PullCollection.find().toArray();
+      console.log(AllPUll);
+      res.send(AllPUll);
+    });
 
-    //  load all jobs 
+    //  load all jobs
 
     app.get("/api/v1/jobs", async (req, res) => {
       // Fallback route when req.params.section is falsy
@@ -243,44 +292,33 @@ async function run() {
     app.get("/api/v1/job/:id", async (req, res) => {
       try {
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await JobsCollection.findOne(query)
-        res.send(result)
+        const query = { _id: new ObjectId(id) };
+        const result = await JobsCollection.findOne(query);
+        res.send(result);
       } catch (error) {
-        console.error('Error from get api job:', error)
-        res.status(500).send("Internal server error")
+        console.error("Error from get api job:", error);
+        res.status(500).send("Internal server error");
       }
+    });
 
-    })
-    app.post("/api/v1/jobs", async (req, res) => {
-      try {
-        const body = req.body;
-        const result = await JobsCollection.insertOne(body)
-        res.send(result)
-      } catch (error) {
-        console.error('Error from post api job:', error)
-        res.status(500).send("Internal server error from post api job")
-      }
-    })
     app.delete("/api/v1/jobs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         console.log(id);
-        const query = { _id: new ObjectId(id) }
-        console.log('from query', query);
-        const result = await JobsCollection.deleteOne(query)
+        const query = { _id: new ObjectId(id) };
+        console.log("from query", query);
+        const result = await JobsCollection.deleteOne(query);
         res.send(result);
       } catch (error) {
-        console.error('Error from delete api job:', error)
-        res.status(500).send("Internal server error from delete api job")
+        console.error("Error from delete api job:", error);
+        res.status(500).send("Internal server error from delete api job");
       }
-  
-    })
+    });
     app.patch("/api/v1/jobs/:id", async (req, res) => {
       try {
         const id = req.params.id;
-        const filter = { _id: new ObjectId(id) }
-        const options = { upsert: true }
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
         const modifiedData = req.body;
         const updatedDoc = {
           $set: {
@@ -290,16 +328,19 @@ async function run() {
             date: modifiedData.date,
             section: modifiedData.section,
             jobUrl: modifiedData.jobsurl,
-          }
-        }
-        const result = await JobsCollection.updateOne(filter, updatedDoc, options)
-        res.send(result)
+          },
+        };
+        const result = await JobsCollection.updateOne(
+          filter,
+          updatedDoc,
+          options
+        );
+        res.send(result);
       } catch (error) {
-        console.error('Error from patch api job:', error)
-        res.status(500).send("Internal server error from patch api job")
+        console.error("Error from patch api job:", error);
+        res.status(500).send("Internal server error from patch api job");
       }
- 
-    })
+    });
     // get bookemark data
 
     app.get("/Bookmark/:email", verifyToken, async (req, res) => {
@@ -360,10 +401,34 @@ async function run() {
       res.send(result);
     });
 
+    // Route to get a poll by ID
+app.get('/polls/:pollId', verifyToken, async (req, res, next) => {
+  try {
+    const { pollId } = req.params;
+
+    // Find the poll in the database by ID
+    const foundPoll = await PullCollection.findOne({
+      _id: new ObjectId(pollId),
+    });
+
+    // If the poll is found, send it in the response
+    if (foundPoll) {
+      return res.status(200).json(foundPoll);
+    }
+
+    // If the poll is not found, return a 404 status
+    res.status(404).json({ message: 'Poll not found' });
+  } catch (error) {
+    // Pass any errors to the error handling middleware
+    next(error);
+  }
+});
+
+
     //  All section Update Method
 
     // Bookmark Update
-    app.put("/News/:id", verifyToken,verifyAdmin, async (req, res) => {
+    app.put("/News/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const updateNews = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -401,6 +466,64 @@ async function run() {
       res.send(result);
     });
 
+    // Route to update a poll
+    // app.patch("/updatePoll/:pollId", async (req, res, next) => {
+    //   try {
+    //     const { pollId } = req.params;
+    //     const { body } = req;
+    //     console.log(body);
+    //     // Prepare updated poll object with default values
+    //     const updatedPoll = {
+    //       ...body,
+    //       options: body?.options.map((option) => ({
+    //         ...option,
+    //         _id: option._id ? option._id : new ObjectId(),
+    //         votes: option.votes ? option.votes : [],
+    //       })),
+    //     };
+    //     console.log(updatedPoll);
+    //     // Update the poll in the database
+    //     const updateResult = await PullCollection.updateOne(
+    //       { _id: new ObjectId(pollId) },
+    //       { $set: updatedPoll },
+    //       { upsert: true }
+    //     );
+    //     res.send(updateResult);
+    //   } catch (error) {}
+    // });
+
+    // Route to update poll votes
+    app.patch("/updatePoll/:pollId", async (req, res, next) => {
+      try {
+        const { pollId } = req.params;
+        const { userId, options } = req.body;
+    
+        const poll = await PullCollection.findOne({ _id: new ObjectId(pollId) });
+    
+        const updatedOptions = poll.options.map((option) => {
+          if (option._id.toString() === options) {
+            if (!option.votes.includes(userId)) {
+              option.votes.push(userId);
+            }
+          } else {
+            option.votes = option.votes.filter((vote) => vote !== userId);
+          }
+          return option;
+        });
+    
+        console.log("update",updatedOptions);
+        const result = await PullCollection.updateOne(
+          { _id: new ObjectId(pollId) },
+          { $set: { options: updatedOptions } }
+        );
+    console.log("result",result);
+        res.status(200).json(result);
+      } catch (error) {
+        next(error);
+      }
+    });
+    
+
     // Use Delete Method
     // admin delete news
     app.delete("/AllNews/:id", verifyToken, verifyAdmin, async (req, res) => {
@@ -416,16 +539,16 @@ async function run() {
       const result = await DonationRequestCollection.deleteOne(query);
       res.send(result);
     });
-//booksmarks delete function--------->
-app.delete("/bookmarks/:id", async(req, res) => {
-  const id = req.params.id;
-  console.log(id)
-  const query = {
-      newsid: id
-  };
-  const Deletebookmarks = await BookmarksCollection.deleteOne(query);
-  res.send(Deletebookmarks)
-})
+    //booksmarks delete function--------->
+    app.delete("/bookmarks/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {
+        newsid: id,
+      };
+      const Deletebookmarks = await BookmarksCollection.deleteOne(query);
+      res.send(Deletebookmarks);
+    });
     app.delete("/Jobs/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
