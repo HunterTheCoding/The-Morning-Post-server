99ca984554eb3196514c11ec794af44d5efa6708
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-02-25 15:30:46 +0600

    remove console log and live Link set section modify

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(
     credentials: true,
   })
 );
-console.log(process.env.STRIPE_SECRET);
 
 app.use(cookieParser());
 
@@ -67,9 +66,7 @@ async function run() {
       const email = req.user?.email;
       const query = { Email: email };
       const user = await UserCollection.findOne(query);
-
       const IsAdmin = user.role === "admin";
-
       if (!IsAdmin) {
         return res.status(401).send({ message: "unauthorized User" });
       } else {
@@ -77,12 +74,9 @@ async function run() {
       }
     };
 
-    // Use Post Method
-
     // JsonWebToken
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-
       const token = jwt.sign(user, process.env.NEWS_ACCESS_TOKEN, {
         expiresIn: "1d",
       });
@@ -100,36 +94,31 @@ async function run() {
 
     app.post("/logout", async (req, res) => {
       const user = req.body;
-
       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
     app.post("/bookmarks", async (req, res) => {
       const newsinfo = req.body;
-
       const result = await BookmarksCollection.insertOne(newsinfo);
       res.send(result);
     });
     //   Create News Section
     app.post("/News", async (req, res) => {
       const News = req.body;
-
       const result = await NewsCollection.insertOne(News);
-
       res.send(result);
     });
-     app.post("/bookmarks", async (req, res) => {
+    app.post("/bookmarks", async (req, res) => {
       const newsinfo = req.body;
       // console.log(result);
       const result = await BookmarksCollection.insertOne(newsinfo);
       res.send(result);
     });
-    
+
     // get bookemark data
     app.get("/Bookmark/:email", verifyToken, async (req, res) => {
       const userEmail = req?.user?.email;
       const reqEmail = req?.params?.email;
-
       // Ensure the requested email matches the user's email
       try {
         // Find all bookmarks for the user
@@ -137,7 +126,6 @@ async function run() {
           useremail: reqEmail,
         };
         const userBookmarks = await BookmarksCollection.find(query).toArray();
-
         // Extract news IDs from bookmarks
         const newsIds = userBookmarks.map(
           (bookmark) => new ObjectId(bookmark.newsid)
@@ -146,7 +134,6 @@ async function run() {
         // Retrieve news data for the bookmarked news IDs
         const newsQuery = { _id: { $in: newsIds } };
         const bookmarkedNews = await NewsCollection.find(newsQuery).toArray();
-
         res.send(bookmarkedNews);
       } catch (error) {
         console.error("Error retrieving bookmarked news:", error);
@@ -156,7 +143,6 @@ async function run() {
     //   Create User Section
     app.post("/users", async (req, res) => {
       const User = req.body;
-
       const query = { Email: User?.Email };
       const Exitinguser = await UserCollection.findOne(query);
 
@@ -164,16 +150,13 @@ async function run() {
         return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await UserCollection.insertOne(User);
-
       return res.send(result);
     });
 
     // create donation
     app.post("/donation-request", verifyToken, async (req, res) => {
       const DonationRequest = req.body;
-
       const result = await DonationRequestCollection.insertOne(DonationRequest);
-
       return res.send(result);
     });
 
@@ -181,7 +164,6 @@ async function run() {
 
     app.post("/create-payment-intent", async (req, res) => {
       const { amount } = req.body;
-
       const amountInCents = parseInt(amount);
       // Create a PaymentIntent with the order amount and currency
       const paymentIntent = await stripe.paymentIntents.create({
@@ -189,7 +171,6 @@ async function run() {
         currency: "usd",
         payment_method_types: ["card"],
       });
-
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
@@ -211,8 +192,6 @@ async function run() {
     app.post("/Polls", async (req, res, next) => {
       try {
         const { body } = req;
-
-        // Check if request body is valid
         if (!body) {
           return res.status(400).json({ message: "Invalid body" });
         }
@@ -229,7 +208,6 @@ async function run() {
 
         // Insert the new poll into the database
         const createdPoll = await PullCollection.insertOne(newPoll);
-
         res.send(createdPoll);
       } catch (error) {
         // Pass any errors to the error handling middleware
@@ -237,10 +215,7 @@ async function run() {
       }
     });
 
-    // Use Get Method
-
     // Banner Section
-
     app.get("/bannar", async (req, res) => {
       const result = await BannarCollection.find().toArray();
       res.send(result);
@@ -256,8 +231,7 @@ async function run() {
           // Query MongoDB collection
           const query = { "newDonation.email": reqemail };
           const result = await DonationRequestCollection.find(query).toArray(); // Invoke toArray as a method
-
-          res.send(result );
+          res.send(result);
         } catch (error) {
           res.status(500).send("Internal Server Error");
         }
@@ -291,16 +265,13 @@ async function run() {
     app.get("/singleNews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-
       const result = await NewsCollection.findOne(query);
-
       res.send(result);
     });
 
     //  get  All Pull request
     app.get("/Show-Pull", verifyToken, async (req, res) => {
       const AllPUll = await PullCollection.find().toArray();
-      console.log(AllPUll);
       res.send(AllPUll);
     });
 
@@ -361,15 +332,14 @@ async function run() {
         res.status(500).send("Internal server error from patch api job");
       }
     });
-    
+
 
     // Check Admin
 
     app.get("/admin/:email", verifyToken, async (req, res) => {
-      // console.log("asoe hlit hocche", req?.user?.email);
       const email = req.params.email;
 
-      // console.log(req?.user, "emaillllllll", email);
+      // console.log(req?.user, "email", email);
       if (email !== req?.user?.email) {
         return res.status(403).send({ message: "unauthorized Access" });
       }
@@ -380,16 +350,13 @@ async function run() {
       if (user?.role === "admin") {
         isAdmin = true;
       }
-      console.log(isAdmin, "admin");
       res.send({ isAdmin });
     });
 
     // show all user
     app.get("/Users", verifyToken, verifyAdmin, async (req, res) => {
       console.log("cheack to token", req?.user?.email);
-      // console.log(req.user);
       const result = await UserCollection.find().toArray();
-      // console.log(result);
       res.send(result);
     });
 
@@ -397,17 +364,14 @@ async function run() {
     app.get("/polls/:pollId", verifyToken, async (req, res, next) => {
       try {
         const { pollId } = req.params;
-
         // Find the poll in the database by ID
         const foundPoll = await PullCollection.findOne({
           _id: new ObjectId(pollId),
         });
-
         // If the poll is found, send it in the response
         if (foundPoll) {
           return res.status(200).json(foundPoll);
         }
-
         // If the poll is not found, return a 404 status
         res.status(404).json({ message: "Poll not found" });
       } catch (error) {
@@ -424,11 +388,6 @@ async function run() {
       const updateNews = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      console.log("id", id);
-      console.log("filter", filter);
-
-      console.log(updateNews);
-
       const Donation = {
         $set: {
           section: updateNews?.section,
@@ -436,60 +395,29 @@ async function run() {
           source: updateNews?.source,
           Upazila: updateNews?.Upazila,
           date: updateNews?.date,
-
           title: updateNews?.title,
           writer: updateNews?.writer,
           hospital_name: updateNews?.hospital_name,
           image: updateNews?.image,
-
           summary: updateNews?.summary,
           news: updateNews?.news,
         },
       };
-      console.log(Donation);
 
       const result = await DonationRequestCollection.updateOne(
         filter,
         Donation,
         options
       );
-      console.log(result);
       res.send(result);
     });
 
-    // Route to update a poll
-    // app.patch("/updatePoll/:pollId", async (req, res, next) => {
-    //   try {
-    //     const { pollId } = req.params;
-    //     const { body } = req;
-    //     console.log(body);
-    //     // Prepare updated poll object with default values
-    //     const updatedPoll = {
-    //       ...body,
-    //       options: body?.options.map((option) => ({
-    //         ...option,
-    //         _id: option._id ? option._id : new ObjectId(),
-    //         votes: option.votes ? option.votes : [],
-    //       })),
-    //     };
-    //     console.log(updatedPoll);
-    //     // Update the poll in the database
-    //     const updateResult = await PullCollection.updateOne(
-    //       { _id: new ObjectId(pollId) },
-    //       { $set: updatedPoll },
-    //       { upsert: true }
-    //     );
-    //     res.send(updateResult);
-    //   } catch (error) {}
-    // });
 
     // Route to update poll votes
-
     app.patch("/updatePoll/:pollId", async (req, res, next) => {
       try {
         const { pollId } = req.params;
         const { userId, options } = req.body;
-
         const poll = await PullCollection.findOne({
           _id: new ObjectId(pollId),
         });
@@ -504,13 +432,10 @@ async function run() {
           }
           return option;
         });
-
-        console.log("update", updatedOptions);
         const result = await PullCollection.updateOne(
           { _id: new ObjectId(pollId) },
           { $set: { options: updatedOptions } }
         );
-        console.log("result", result);
         res.status(200).json(result);
       } catch (error) {
         next(error);
@@ -535,7 +460,6 @@ async function run() {
     //booksmarks delete function--------->
     app.delete("/bookmarks/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = {
         newsid: id,
       };
@@ -551,9 +475,7 @@ async function run() {
     app.get("/singlenews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      // console.log(id)
       const result = await NewsCollection.findOne(query);
-      // console.log(result);
       res.send(result);
     });
 
@@ -563,21 +485,24 @@ async function run() {
       const live = await LiveLink.find().toArray();
       res.send(live);
     });
-    app.post("/live", async (req, res) => {
+    app.put("/live", async (req, res) => {
       try {
         const body = req.body;
-        const result = await LiveLink.insertOne(body);
+        const filter = { Find: body?.Find };
+        const options = { upsert: true };
+        const Link = {
+          $set: {
+            Link: body?.Link,
+            Find: body?.Find,
+          },
+        };
+        const result = await LiveLink.updateOne(filter,Link,options)
         res.send(result);
       } catch (error) {
         res.status(500).send("Internal server error from post api job");
       }
     });
-    app.delete("/live/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await LiveLink.deleteOne(query);
-      res.send(result);
-    });
+
 
     await client.connect();
 
